d0afc196f690e5fc66da75da7bf26596fe483e9a
Joel Byler <joelbyler@gmail.com>
2012-09-21 21:48:08 -0400

    additional WIP changes, not ready to release

--- lib/svg.rb
+++ lib/svg.rb
@@ -18,4 +18,17 @@ module Svg
     end
   end
   
+  #<rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)"/>
+  #<rect x="50" y="20" width="150" height="150" style="fill:blue; stroke:pink; stroke-width:5; fill-opacity:0.1; stroke-opacity:0.9"/>
+  #<rect x="50" y="20" width="150" height="150" style="fill:blue;stroke:pink;stroke-width:5;opacity:0.5"/>
+  #<rect x="50" y="20" rx="20" ry="20" width="150" height="150" style="fill:red;stroke:black;stroke-width:5;opacity:0.5"/>  
+
+  #<ellipse cx="300" cy="80" rx="100" ry="50" style="fill:yellow;stroke:purple;stroke-width:2"/>
+  #<ellipse cx="240" cy="100" rx="220" ry="30" style="fill:purple"/>
+  #<ellipse cx="220" cy="70" rx="190" ry="20" style="fill:lime"/>
+  #<ellipse cx="210" cy="45" rx="170" ry="15" style="fill:yellow"/>
+  #<ellipse cx="240" cy="50" rx="220" ry="30" style="fill:yellow"/>
+  #<ellipse cx="220" cy="50" rx="190" ry="20" style="fill:white"/>
+  
+  #<line x1="0" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2"/>
 end
